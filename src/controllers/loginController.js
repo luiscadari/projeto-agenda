393e8d66a8c5663048c8c9a48382d3cc9cde69d7
luiscadari <luiscadari01@gmail.com>
2024-12-27 09:18:09 -0300

    :sparkles: feat: login.register

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -3,7 +3,21 @@ exports.index = (req, res) => {
     res.render('login')
 }
 
-exports.register = (req, res) => {
-    const login = new Login(req.body)
-    res.send(login.register())
+exports.register = async (req, res) => {
+    try{
+        const login = new Login(req.body)
+        await login.register();
+        if(login.errors.length > 0){
+            req.flash('errors', login.errors);
+            req.session.save(()=>{
+                return res.redirect('/login');
+            })
+            return;
+        }
+    req.flash('success', 'Seu usuário foi registrado com sucesso!')
+        return res.send(login.errors)
+    }catch(e){
+        console.log(e);
+        return res.render('404');
+    }
 }
--- src/models/loginModel.js
+++ src/models/loginModel.js
@@ -18,26 +18,38 @@ class Login {
   valida() {
     // Validação dos campos;
     this.cleanUp();
+    console.log("Body que chegou no valida: ", this.body)
 
     // Valida o email;
-    if(validator.isEmail(this.body.email)){
+    if(!validator.isEmail(this.body.email)){
+      console.log("Email inválido")
       this.errors.push('Email inválido')
     }
     //valida a senha
     if(this.body.password.length < 3 || this.body.password.length > 50){
+      console.log('senha inválida')
+      console.log("password: ", this.body.password);
+      console.log("password.length", this.body.password.length)
       this.errors.push("A senha precisa ter entre 3 e 50 caracteres")
     }
   }
   async register() {
+    console.log('Dados que chegaram: ', this.body)
     this.valida();
     if(this.errors.length > 0){
       return
     }
-    await LoginModel.create(this.body);
+    try{
+      this.user = await LoginModel.create(this.body);
+    }catch(e){
+      console.log(e);
+    }
+
   }
   cleanUp() {
+    this.errors = []
     for(const key in this.body){
-      if(typeof this.body[key !== 'string']){
+      if(typeof this.body[key] !== 'string'){
         this.body[key] = '';
       }
     }
